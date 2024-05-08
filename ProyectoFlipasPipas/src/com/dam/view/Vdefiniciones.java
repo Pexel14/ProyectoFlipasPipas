ad9fb92601b7073105ebc0620f52b071cb10fa3d
Alejandro <aaleex8842@gmail.com>
2024-05-08 19:06:53 +0200

    add definiciones

---
+++ ProyectoFlipasPipas/src/com/dam/view/Vdefiniciones.java
@@ -0,0 +1,44 @@
+package com.dam.view;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JTextArea;
+
+public class Vdefiniciones extends JFrame {
+	
+	private static final int ANCHO = 265;
+	private static final int ALTO = 270;
+	private JTextArea txtInfo;
+	public Vdefiniciones() {
+		super("definición");
+				
+		configurarFrame();
+		componentes();		
+		
+	}
+
+	private void configurarFrame() {
+		
+		setSize(265, 270);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		getContentPane().setBackground(new Color(50, 50, 75));	
+		
+	}
+
+	private void componentes() {
+		
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		txtInfo = new JTextArea();
+		scrollPane.setViewportView(txtInfo);
+		
+	}
+	
+	public void mostrarVentana() {
+		setVisible(true);
+	}
+}
