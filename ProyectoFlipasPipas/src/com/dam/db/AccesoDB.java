09b35c6d176b2a1a1a419e575fc297c51c18598e
Axel Leon <axeleon76@gmail.com>
2024-05-08 17:48:37 +0200

    Modificado Conector BBDD

--- ProyectoFlipasPipas/src/com/dam/db/AccesoDB.java
+++ ProyectoFlipasPipas/src/com/dam/db/AccesoDB.java
@@ -1,8 +1,13 @@
 package com.dam.db;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class AccesoDB {
 
@@ -10,9 +15,23 @@ public class AccesoDB {
 	private String url;
 	
 	public AccesoDB() {
-		this.driver = "org.sqlite.JDBC";
-		this.url = "jdbc:sqlite:db/BD_Prueba.db";
+		Properties prop = new Properties();
+		InputStream is = null;
+		
+		try {
+			is = new FileInputStream("db/configuracionDB.properties");
+			prop.load(is);
+			
+			driver = prop.getProperty("DRIVER");
+			url = prop.getProperty("URL");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
 	
 	public Connection getConexion() throws ClassNotFoundException, SQLException{
 		Class.forName(driver);
